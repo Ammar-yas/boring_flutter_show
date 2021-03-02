63268837497444ec65177a2d9a49dc992bf29490
Ammar Yasser <ammar.yasser@mondia.com>
2021-03-02 10:05:31 +0200

    Initial commit

---
+++ boring_flutter_show/android/app/src/main/kotlin/dev/ammaryasser/boring_flutter_show/MainActivity.kt
@@ -0,0 +1,6 @@
+package dev.ammaryasser.boring_flutter_show
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ boring_flutter_show/lib/parse_json.dart
@@ -0,0 +1,9 @@
+import 'package:boring_flutter_show/data/articles.dart';
+
+List<int> parseTopStories(String json) {
+  throw UnimplementedError();
+}
+
+Article parseArticle(String json) {
+  throw UnimplementedError();
+}
